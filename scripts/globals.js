1c3d3a4b363429800be15b0006a814a0c3fc3adf
Hoàng Loan <Hoangloan1898hust@gmail.com>
2019-06-07 01:59:15 +0700

    Update globals.js

--- scripts/globals.js
+++ scripts/globals.js
@@ -5,11 +5,11 @@ const RESOURCES = {
         names: ['Role']
     },
     POST: { id: 1, names: ['Blog Post'],
-        maxImageSize: 1, maxAttachSize: 2, //MB
+        maxImageSize: 1, maxAttachSize: 1, //MB
         adminPageSize: 5, userPageSize: 12, recentPageSize: 10,
         defaultImage: 'images/default-image.png'
     },
-    CATEGORY: { id: 2, names: ['Categories'], pageSize: 5, postPageSize: 3 },
+    CATEGORY: { id: 2, names: ['Categories'], pageSize: 5, postPageSize: 10 },
     USER: { id: 3, names: ['User'], pageSize: 5 },
     PERMISSION: { id: 4, names: ['Priviledge'] }
 };
